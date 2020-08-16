24e996467f8aa4be29088f209387358c668c80a8
jommiejom <jombhanut@gmail.com>
2020-08-16 16:51:26 +0700

    P/C components

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#news">News</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li><a href="#about">About</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer>
+            <p>Author: Moshi</p>
+            <p><a href="mailto:moshi@example.com">mail@example.com</a></p>
+        </footer>
+    )
+}
+
+export default Footer
