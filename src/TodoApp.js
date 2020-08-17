8e1cc1727325296fb5129dc206a0e8f295574700
jommiejom <jombhanut@gmail.com>
2020-08-17 14:24:45 +0700

    Todo Phase1

---
+++ src/TodoApp.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function TodoApp() {
+    return (
+        <div>
+            <input type="checkbox" />
+            <p>Task to do </p>
+
+            <input type="checkbox" />
+            <p>Task to do </p>
+
+            <input type="checkbox" />
+            <p>Task to do </p>
+        </div>
+    )
+}
+
+export default TodoApp
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import element from './components/MyInfo'
 import App from './App'
+import TodoApp from './TodoApp'
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<TodoApp />, document.getElementById("root"))
